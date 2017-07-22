fd910a8435d89ec28405070dda09edd0732af7bb
Shree Ranga <shreerangaraju@gmail.com>
2017-07-22 17:18:40 -0400

    minor changes to load

--- mnist_web/model/load.py
+++ mnist_web/model/load.py
@@ -6,7 +6,10 @@ Load the trained model
 
 # import libraries
 import keras
+import keras.models
 import tensorflow as tf
+import numpy as np
+from scipy.misc import imread, imresize,imshow
 from keras.models import model_from_json
 
 def init():
@@ -17,12 +20,13 @@ def init():
     loaded_model = model_from_json(loaded_model_json)
 
     # load weights into new model
-    loaded_model.load_weights('model.h5')
+    loaded_model.load_weights("model.h5")
     print "Loaded model from the disk"
 
     # compile the newly loaded model
     loaded_model.compile(loss=keras.losses.categorical_crossentropy,
-        optimizer=keras.optimizers.adam())
+        optimizer=keras.optimizers.adam(),
+        metrics =['accuracy'])
     graph = tf.get_default_graph()
 
     return loaded_model, graph
